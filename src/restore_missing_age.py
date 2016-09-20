d35ceb852911c3b1b87231a951058db4c75593c2
guangleiw <stylite.wang@gmail.com>
2016-09-20 23:08:35 +0800

    123

--- src/restore_missing_age.py
+++ src/restore_missing_age.py
@@ -13,9 +13,12 @@ def restore_missing_age(df):
     known_age = age_df[age_df.Age.notnull()].as_matrix()
     unknown_age = age_df[age_df.Age.isnull()].as_matrix()
 
-    y = known_age[:,0]
+    #print(type(known_age))
 
+    y = known_age[:,0]
+    #print(y)
     X = known_age[:,1:]
+    #print(X)
 
     rfr = RandomForestRegressor(random_state=0,n_estimators=2000,n_jobs=-1)
     rfr.fit(X,y)
@@ -35,4 +38,15 @@ def set_Cabin_type(df):
 
 data_train, rfr = restore_missing_age(data_train)
 data_train = set_Cabin_type(data_train)
-print(data_train)
+
+dummies_Cabin = pd.get_dummies(data_train['Cabin'], prefix= 'Cabin')
+
+dummies_Embarked = pd.get_dummies(data_train['Embarked'], prefix= 'Embarked')
+
+dummies_Sex = pd.get_dummies(data_train['Sex'], prefix= 'Sex')
+
+dummies_Pclass = pd.get_dummies(data_train['Pclass'], prefix= 'Pclass')
+
+df = pd.concat([data_train, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+print(df)
